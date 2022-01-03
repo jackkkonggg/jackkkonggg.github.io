e0345d4393e8cfa73190f34ba7302eba417a56b5
jackkkonggg <jqong@connect.ust.hk>
2022-01-03 10:03:48 +0800

    feat: add image

--- src/components/Main/About.tsx
+++ src/components/Main/About.tsx
@@ -22,30 +22,37 @@ export const About: FC = () => {
   return (
     <section className="py-24">
       <AutoNumberedHeading heading="About Me" />
-      <div className="space-y-4 text-slate-dark">
-        <p>
-          Hello! My name is Jack and I enjoy creating things that live on the
-          internet. My interest in web development started back in 2020 when I
-          decided to try editing custom Tumblr themes — turns out hacking
-          together a custom reblog button taught me a lot about HTML & CSS!
-        </p>
-        <p>
-          Fast-forward to today, and I’ve had the privilege of working at an
-          advertising agency, a start-up, a huge corporation, and a student-led
-          design studio. My main focus these days is building accessible,
-          inclusive products and digital experiences at EventX for a variety of
-          clients.
-        </p>
-        <p>Here are a few technologies I’ve been working with recently:</p>
-        <Technologies
-          technologies={[
-            'JavaScript (ES6+)',
-            'TypeScript',
-            'React',
-            'Next.js',
-            'TypeORM',
-            'Solidity',
-          ]}
+      <div className="grid grid-cols-[minmax(0,_1fr)_200px] gap-x-10">
+        <div className="space-y-4 text-slate-dark">
+          <p>
+            Hello! My name is Jack and I enjoy creating things that live on the
+            internet. My interest in web development started back in 2020 when I
+            decided to try editing custom Tumblr themes — turns out hacking
+            together a custom reblog button taught me a lot about HTML & CSS!
+          </p>
+          <p>
+            Fast-forward to today, and I’ve had the privilege of working at an
+            advertising agency, a start-up, a huge corporation, and a
+            student-led design studio. My main focus these days is building
+            accessible, inclusive products and digital experiences at EventX for
+            a variety of clients.
+          </p>
+          <p>Here are a few technologies I’ve been working with recently:</p>
+          <Technologies
+            technologies={[
+              'JavaScript (ES6+)',
+              'TypeScript',
+              'React',
+              'Next.js',
+              'TypeORM',
+              'Solidity',
+            ]}
+          />
+        </div>
+        <img
+          className="rounded"
+          src="https://media-exp1.licdn.com/dms/image/C5603AQH3gsHrqBq_TQ/profile-displayphoto-shrink_800_800/0/1638520282735?e=1646870400&v=beta&t=Skk1qKVHAZmss6B8D5jhriePbEugoLaIb7PKXEfQRQo"
+          alt="Profile Photo"
         />
       </div>
     </section>
