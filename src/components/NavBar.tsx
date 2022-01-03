cca8211c4a0164742301573e7c94c4f155250399
jackkkonggg <jqong@connect.ust.hk>
2022-01-03 22:06:54 +0800

    feat: add logic into nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,52 @@
-/* eslint-disable @next/next/no-img-element */
 import { Avatar } from '@/design-system/Avatar';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Button } from '@/design-system/Button';
 import { Link } from '@/design-system/Link';
+import throttle from 'lodash.throttle';
+import clsx from 'clsx';
+
+let lastScrollTop = 0;
+const IS_AT_TOP_PX = 100;
 
 export const NavBar: FC = () => {
+  const [isNavBarShown, setNavBarShown] = useState(true);
+  const [isScrollAtTop, setScrollAtTop] = useState(true);
+
+  useEffect(() => {
+    const shouldShowNavBar = (currentScrollTop: number) => {
+      if (currentScrollTop < IS_AT_TOP_PX) {
+        return true;
+      }
+
+      const isScrollingDown = currentScrollTop < lastScrollTop;
+      return isScrollingDown;
+    };
+
+    const handleScroll = throttle(() => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      setNavBarShown(shouldShowNavBar(scrollTop));
+      setScrollAtTop(scrollTop < IS_AT_TOP_PX);
+      lastScrollTop = scrollTop;
+    }, 250);
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed top-0 py-8 px-12 w-full text-sm sm:px-6 lg:px-10">
+    <header
+      className={clsx(
+        'fixed top-0  w-full text-sm sm:px-10 lg:px-10',
+        'backdrop-blur bg-[rgba(10,25,47,0.85)] z-10 transition-all duration-500',
+        isScrollAtTop ? 'py-8' : 'py-4',
+        !isScrollAtTop && 'nav-shadow',
+        !isNavBarShown && '-translate-y-full',
+      )}
+    >
       <nav className="flex items-center w-full font-mono text-slate-light">
         <a className="flex-grow">
           <Avatar>
@@ -14,8 +54,8 @@ export const NavBar: FC = () => {
           </Avatar>
         </a>
         <ol className="flex items-center justify-between counter-reset">
-          {['About', 'Experience', 'Work', 'Contact'].map((item, index) => (
-            <li key="item" className="px-6 py-4 counter-increment">
+          {['About', 'Experience', 'Work', 'Contact'].map((item) => (
+            <li key={item} className="px-6 py-4 counter-increment">
               <Link
                 href="#"
                 className="before-green-counter before:mr-1 text-slate"
