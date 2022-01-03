427f3f13aeec41626bfbf3a020b859aaa7ef7a2e
jackkkonggg <jqong@connect.ust.hk>
2022-01-03 23:44:32 +0800

    chore: github pages config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const IS_PROD = process.env.NODE_ENV === 'production';
+const GITHUB_REPO_NAME = 'personal-portfolio';
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix: IS_PROD ? `/${GITHUB_REPO_NAME}/` : '',
+};
