a7aed0d295ff16ebd85994a2df844af0f6ca815a
jackkkonggg <jqong@connect.ust.hk>
2022-01-04 00:22:36 +0800

    feat: deploy to index route

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-const IS_PROD = process.env.NODE_ENV === 'production';
-const GITHUB_REPO_NAME = 'personal-portfolio';
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: IS_PROD ? `/${GITHUB_REPO_NAME}/` : '',
 };
