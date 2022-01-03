3ee95d8508c9622d9d2714f979289da0f608524a
jackkkonggg <jackkkonggg@users.noreply.github.com>
2022-01-03 16:23:42 +0000

    deploy: a7aed0d295ff16ebd85994a2df844af0f6ca815a

---
+++ _next/static/tenypUSr6feS-n3pWUOU3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/164-334c118ed01de8b4.js","static/chunks/pages/index-603a69a2eb498200.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
