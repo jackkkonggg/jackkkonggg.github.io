3c4d890bfca480704c86f9e7ad55a9257a638c0b
jackkkonggg <jackkkonggg@users.noreply.github.com>
2022-01-09 08:06:56 +0000

    deploy: 24fc79aab01129c9e3c2f6370b0d0690c1b1f4be

--- _next/static/ahwO4VlRJvfLYttb3tcu8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/164-334c118ed01de8b4.js","static/chunks/pages/index-52d5186e9868170f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/lTj9dakGx5CouzG-6eUR0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/828-b516415020545339.js","static/chunks/pages/index-71336f1f1b8086b4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
